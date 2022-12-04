66ae2ae010d8bd0b26e9b542346224bc978f3471
Bassam Meshaal <bmeshal@gmail.com>
2022-12-04 14:11:54 -0500

    Initial Springboot project

---
+++ src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabahspringbootsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabahSpringbootSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabahSpringbootSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabahspringbootsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabahSpringbootSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
