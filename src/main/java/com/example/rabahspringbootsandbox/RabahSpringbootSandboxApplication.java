949eb7a7dfc57a2d25c554439e35a33217aaafdf
Bassam Meshaal <bmeshal@gmail.com>
2022-12-04 14:21:12 -0500

    Start connecting with tasks ui

--- src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
+++ src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
@@ -2,12 +2,19 @@ package com.example.rabahspringbootsandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class RabahSpringbootSandboxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabahSpringbootSandboxApplication.class, args);
+    }
+
+    @RequestMapping("/tasks")
+    public String saveTask(String tasks){
+        // write logic to store tasks into db
+        return "Success";
     }
 
 }
