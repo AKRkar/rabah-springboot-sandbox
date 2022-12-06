61db31b3afc5c448e11a6226d78f48c366bada05
Bassam Meshaal <bmeshal@gmail.com>
2022-12-06 12:36:52 -0500

    Start post endpoint for tasks

--- src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
+++ src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
@@ -3,6 +3,7 @@ package com.example.rabahspringbootsandbox;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
@@ -16,8 +17,15 @@ public class RabahSpringbootSandboxApplication {
 
     @GetMapping("/tasks")
     public String getTask(@RequestParam(value = "tasks" )int taskId){ // json
-        log.info("Tasks received: " + taskId);
+        log.info("GET Tasks received: " + taskId);
         // write logic to get task from db
+        return "Success";
+    }
+
+    @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String abcTask(@RequestBody String tasksJsonArray){ // json
+        log.info("POST Tasks received: " + tasksJsonArray);
+        // write logic to store task in db
         return "Success";
     }
 }
