594bacf9925fe61b606c00b1405f15a0330d95f4
Bassam Meshaal <bmeshal@gmail.com>
2022-12-06 11:17:14 -0500

    start get task endpoint

--- src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
+++ src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
@@ -1,20 +1,23 @@
 package com.example.rabahspringbootsandbox;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@Log
 public class RabahSpringbootSandboxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabahSpringbootSandboxApplication.class, args);
     }
 
-    @RequestMapping("/tasks")
-    public String saveTask(String tasks){
-        // write logic to store tasks into db
+    @GetMapping("/tasks")
+    public String getTask(@RequestParam(value = "tasks" )int taskId){ // json
+        log.info("Tasks received: " + taskId);
+        // write logic to get task from db
         return "Success";
     }
-
 }
