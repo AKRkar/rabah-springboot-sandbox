acfdce25f0bd51b405577173d9793bba4cd464d6
Bassam Meshaal <bmeshal@gmail.com>
2022-12-07 13:58:31 -0500

    Added logic to store tasks in memory

--- src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
+++ src/main/java/com/example/rabahspringbootsandbox/RabahSpringbootSandboxApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 @RestController
 @Log
@@ -16,16 +20,44 @@ public class RabahSpringbootSandboxApplication {
     }
 
     @GetMapping("/tasks")
-    public String getTask(@RequestParam(value = "tasks" )int taskId){ // json
+    public String getTaskList(@RequestParam(value = "tasks" )int taskId){ // json
         log.info("GET Tasks received: " + taskId);
         // write logic to get task from db
-        return "Success";
+        return "Success - all tasks returned";
     }
 
     @PostMapping(value = "/tasks", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String abcTask(@RequestBody String tasksJsonArray){ // json
-        log.info("POST Tasks received: " + tasksJsonArray);
+    public String saveTaskList(@RequestBody List<Task> IncomingTaskList){ // json
+        for(Task o : IncomingTaskList){
+            log.info("POST Tasks received - id " + o.id + " : " + o.text);
+            inMemTaskList.put(o.id, o);
+        }
+
+        printMem();
+
         // write logic to store task in db
         return "Success";
     }
+
+    private void printMem() {
+        for (Map.Entry<Long, Task> entry : inMemTaskList.entrySet()) {
+            Long key = entry.getKey();
+            Task task = entry.getValue();
+            log.info("Task: " + task.id + " - " + task.text);
+        }
+    }
+
+    public Map<Long, Task> inMemTaskList = new HashMap<>();
+
+    public static class Task{
+        public Task(Long id, String text, Boolean done){
+            this.id = id;
+            this.text = text;
+            this.done = done;
+        }
+        public Long id;
+        public String text;
+        public Boolean done;
+    }
+
 }
